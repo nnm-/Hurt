c73d3e77d9fa5a988fef2847b3811357a4866cab
nnm- <vvg.nnm@gmail.com>
2012-07-10 15:24:44 +0400

    валидация длины, мыло как рекваред поле

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   
-  attr_accessible :username, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   
   validates :username, presence: true, uniqueness: true
+  
+  validates :password, :length  => { :within => 6..40 }
   
 end
