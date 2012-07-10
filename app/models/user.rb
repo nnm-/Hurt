3d550eb39957af060b597fda709dbd4028d60be0
nnm- <vvg.nnm@gmail.com>
2012-07-10 16:18:27 +0400

    validation email regexp, username length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   
   attr_accessible :username, :email, :password, :password_confirmation
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { within: 3..30 }
   
-  validates :password, :length  => { :within => 6..40 }
+  validates :password, length: { within: 6..40 }
+  validates :email, format: { with: email_regex }
   
 end
