bbaafba95fcf79f5d738917de653565fb343e66b
nnm- <vvg.nnm@gmail.com>
2012-07-10 05:57:45 +0400

    всякий мусор и прочие неинтересности

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120706091333) do
+ActiveRecord::Schema.define(:version => 20120709195415) do
 
   create_table "posts", :force => true do |t|
     t.text     "message"
@@ -19,5 +19,16 @@ ActiveRecord::Schema.define(:version => 20120706091333) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "username",         :null => false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
